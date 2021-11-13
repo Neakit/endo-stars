a386d10ce9d29da36f3d4e2bc45bd8ce2cfadda8
neakit <pavliukny@gmail.com>
2021-11-13 23:32:10 +0300

    add some fake auth method

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { defineComponent } from '@vue/composition-api'
 
-#nav {
-  padding: 30px;
+export default defineComponent({
+  
+  setup() {
+    const login = async () => {
+      const response = await fetch('http://localhost:3000/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+          "email": "nilson@email.com",
+          "password":"nilson"
+        })
+      });
+    };
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    return {
+      login
     }
-  }
-}
+
+  },
+})
+</script>
+
+
+<style lang="scss">
+
 </style>
