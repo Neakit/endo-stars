1f825cfd472a3f4d3b9047a5719eb2501b5da374
neakit <pavliukny@gmail.com>
2021-11-13 23:27:51 +0300

    add new components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,56 @@
+<template>
+    <b-navbar class="as-navbar" toggleable="lg" type="light">
+        <b-container>
+            <Logo class="mr-4" />
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item
+                        class="as-title"  
+                        v-for="(item, index) in navigationItems" 
+                        :to="item.to"
+                        :key="index"
+                    >
+                        {{ item.title }}
+                    </b-nav-item>
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item-dropdown right  no-caret>
+                        <template #button-content>
+                            <Avatar />
+                        </template>
+                        <b-dropdown-item href="#">Profile</b-dropdown-item>
+                        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-container>
+    </b-navbar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import Logo from "@img/logo.svg";
+import Avatar from "@components/Avatar.vue"  
+
+export default defineComponent({
+    components: {
+        Logo,
+        Avatar
+    },
+    props: {
+        navigationItems: {
+            required: true,
+            type: Array
+        }
+    },
+    setup() {
+        return {
+            // navigationItems
+        }
+    },
+})
+</script>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="es-avatar">
+    <ProfileSVG class="es-avatar__svg" />
+    <div class="es-avatar__title">
+        <p class="es-title-h1">A</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import ProfileSVG from "@img/profile.svg"
+
+export default defineComponent({
+    components: {
+        ProfileSVG
+    },
+    setup() {
+        return {
+            //
+        }
+    },
+})
+</script>
