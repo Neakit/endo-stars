a68b22d28647654c2f4e8b2f427eab7562442e0a
neakit <pavliukny@gmail.com>
2021-11-15 18:40:05 +0300

    add custom button

---
+++ src/components/es-button.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-btn v-bind="$attrs">
+      <slot v-if="proccess === 'ready'" />
+      <b-icon v-else-if="proccess === 'loading'" icon="three-dots" animation="cylon" font-scale="4"></b-icon>
+      <b-icon v-else-if="proccess === 'finished'" icon="check-lg"></b-icon>
+    </b-btn>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    props: {
+        proccess: {
+            type: [String],
+            required: false,
+            default: "ready"
+        },
+    },
+    setup(props) {
+        console.log(props);
+    }
+})
+</script>
