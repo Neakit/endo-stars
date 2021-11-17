0991d385fbe6a64e5a37759a1d317adc03138b3d
neakit <pavliukny@gmail.com>
2021-11-17 21:52:48 +0300

    add search input field

---
+++ src/components/es-input-search.vue
@@ -0,0 +1,42 @@
+<template>
+    <b-input-group>
+        <b-form-input 
+            :value="value" 
+            @input="$emit('input', $event)" v-bind="$attrs" 
+            @focus="focusActive = true"    
+            @blur="focusActive = false"    
+        />
+        <b-icon class="search-icon" scale="1.5" icon="search"></b-icon>
+    </b-input-group>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    props: {
+        value: {
+            type: [String],
+            required: true
+        },
+    },
+    setup() {
+        //
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+
+::v-deep {
+    .form-control {
+        position: relative;
+    }
+    .search-icon {
+        position: absolute;
+        right: 14px;
+        top: 28%;
+        z-index: 4;
+    }
+}
+</style>
