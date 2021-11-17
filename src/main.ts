2a583bceeeb4ade5c2b389d6f326f65a0be54b79
neakit <pavliukny@gmail.com>
2021-11-17 20:53:33 +0300

    composition api correct import

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,28 @@
+import './installCompositionApi';
 import Vue from 'vue';
+
 import App from './App.vue';
 import router from '@router/index';
-import VueCompositionAPI from '@vue/composition-api';
 
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import "@style/app.scss";
 
+import RequestManager from '@services/RequestManager';
+
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
-Vue.use(VueCompositionAPI)
 
 import { mask } from "vue-the-mask"
 Vue.directive('mask', mask);
 
 Vue.config.productionTip = false
+
+globalThis.$requestManager = RequestManager;
+global.$requestManager = RequestManager;
+window.$requestManager = RequestManager;
+
+
 
 new Vue({
   router,
