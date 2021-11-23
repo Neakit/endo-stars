2e2230ce14482bc5505357b7d902e7bd8c860a10
neakit <pavliukny@gmail.com>
2021-11-24 01:43:00 +0300

    remove permissions

--- src/services/RequestManager/dashboard/Requisites.ts
+++ src/services/RequestManager/dashboard/Requisites.ts
@@ -13,26 +13,32 @@ export default class Requisites {
   }
 
   getContractorTable(params = {}) {
-    return this.sendRequestService("GET:PRIVATE", "api://440/contractor", { params: params });
+    // return this.sendRequestService("GET:PRIVATE", "api://440/contractor", { params: params });
+    return this.sendRequestService("GET:PRIVATE", "api://contractor", { params: params });
   }
 
   addContractor(data: Contractor) {
-    return this.sendRequestService("POST:PRIVATE", "api://664/contractor", { data: data });
+    // return this.sendRequestService("POST:PRIVATE", "api://664/contractor", { data: data });
+    return this.sendRequestService("POST:PRIVATE", "api://contractor", { data: data });
   }
 
   getEndClientTable(params = {}) {
-    return this.sendRequestService("GET:PRIVATE", "api://440/endclient", { params: params });
+    // return this.sendRequestService("GET:PRIVATE", "api://440/endclient", { params: params });
+    return this.sendRequestService("GET:PRIVATE", "api://endclient", { params: params });
   }
 
   addEndClient(data: any) {
-    return this.sendRequestService("POST:PRIVATE", "api://664/endclient", { data: data });
+    // return this.sendRequestService("POST:PRIVATE", "api://664/endclient", { data: data });
+    return this.sendRequestService("POST:PRIVATE", "api://endclient", { data: data });
   }
 
   getCompanyRequisitesTable(params = {}) {
-    return this.sendRequestService("GET:PRIVATE", "api://440/companyRequisites", { params: params });
+    // return this.sendRequestService("GET:PRIVATE", "api://440/companyRequisites", { params: params });
+    return this.sendRequestService("GET:PRIVATE", "api://companyRequisites", { params: params });
   }
 
   addCompanyRequisites(data: any) {
-    return this.sendRequestService("POST:PRIVATE", "api://664/companyRequisites", { data: data });
+    // return this.sendRequestService("POST:PRIVATE", "api://664/companyRequisites", { data: data });
+    return this.sendRequestService("POST:PRIVATE", "api://companyRequisites", { data: data });
   }
 }
