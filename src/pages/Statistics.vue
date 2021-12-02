aa01bda2f7e19d1f01b6a1f3f884dfc3a1d32551
neakit <pavliukny@gmail.com>
2021-12-02 14:25:07 +0300

    test table2

--- src/pages/Statistics.vue
+++ src/pages/Statistics.vue
@@ -1,13 +1,41 @@
 <template>
   <b-container class="pt-4">
     <p class="es-title-h1 my-5">Компании</p>
-    <VueQuintable :config="config" :rows="rows" v-model="selectedRows">
+    <VueQuintable :config="config" :rows="rows">
       <template v-slot:cell-complete="context">
-        <div class="btn btn-sm btn-info" @click.stop="alert(context.cell.text)">{{ context.cell.text }}</div>
+        <span v-if="context.cell.text">
+          {{ context.cell.text }}
+        </span>
+
+        <div v-else class="btn btn-sm btn-info">{{ context.cell.action }}</div>
       </template>
-      <template v-slot:generated-cell-complete="context">
-        <div class="btn btn-sm btn-info" @click.stop="alert(context.cell.text)">{{ context.cell.text }}</div>
+
+      <template v-slot:sticky-cell-complete="context">
+        <span v-if="context.cell.text">
+          {{ context.cell.text }}
+        </span>
+
+        <div v-else class="btn btn-sm btn-info">{{ context.cell.action }}</div>
       </template>
+      <!-- <template v-slot:header>
+        <p-check class="p-switch" v-model="showRows">Toggle Rows</p-check>
+        <hr />
+        <div class="alert alert-primary">
+          <div class="text-center">
+            <h4>VueQuintable</h4>
+          </div>
+        </div>
+      </template> -->
+      <!-- <template v-slot:footer>
+        <div class="text-center py-3 px-3 mt-3 bg-info text-white">
+          Copyright © 2020 by Quintet Consulting UG (haftungsbeschränkt)
+        </div>
+      </template>
+      <template v-slot:no-results>
+        <div class="text-center">
+          <div class="alert alert-danger">No Results...</div>
+        </div>
+      </template> -->
     </VueQuintable>
   </b-container>
 </template>
@@ -17,37 +45,50 @@ import { defineComponent, computed, ref } from "@vue/composition-api";
 import Chance from "chance";
 
 export default defineComponent({
-  setup() {
-    const selectedRows = ref([]);
+  data() {
+    return {
+      config: {
+        columns: [
+          {
+            alwaysExpanded: true,
+            headline: "Age",
+            breakpoint: "md",
+          },
+          {
+            headline: "Name",
+            alwaysExpanded: true,
+            breakpoint: "md",
+          },
 
-    const config = {
-      columns: [
-        {
-          headline: "Name",
-          breakpoint: "md",
-          alwaysExpanded: true,
-        },
-        {
-          headline: "Age",
-          breakpoint: "md",
-          alwaysExpanded: true,
-        },
-        {
-          headline: "Birth Place",
-          breakpoint: "md",
-          alwaysExpanded: true,
-        },
-        {
-          headline: "Job",
-          breakpoint: "md",
-          alwaysExpanded: true,
-        },
-      ],
-      selectPosition: "pre",
-      select: true,
+          {
+            alwaysExpanded: true,
+            headline: "Birth Place",
+            breakpoint: "md",
+          },
+          {
+            alwaysExpanded: true,
+            headline: "Job",
+            breakpoint: "md",
+          },
+          {
+            alwaysExpanded: true,
+            headline: " ",
+            breakpoint: "md",
+          },
+        ],
+        selectPosition: "pre",
+        select: true,
+      },
+
+      showRows: true,
     };
+  },
+  computed: {
+    rows() {
+      if (!this.showRows) {
+        return [];
+      }
 
-    const rows = computed(() => {
       let count = 10;
       const rows = [];
 
@@ -67,17 +108,19 @@ export default defineComponent({
           {
             text: chance.profession(),
           },
+          {
+            action: chance.profession(),
+          },
         ]);
       }
 
       return rows;
-    });
-
-    return {
-      config,
-      rows,
-      selectedRows,
-    };
+    },
+  },
+  methods: {
+    alert(text) {
+      alert("I clicked the slot " + text);
+    },
   },
 });
 </script>
